f9c85b4a8f59c74492eb9ae6578e9c811baaa8af
Bernd Reinhold <hold-und-rein@web.de>
2016-01-11 21:46:17 -1000

    seaborn for correlation matrix -> works nicely
    modified plotting.py in pandas/tools/ to produce hexbin-plot in scatter_matrix()
    
    function corr_matrix() newly introduced, it just calls seaborn.heatmap
    
    added options to append in body_content(), corr_matrix(), corr_plots()
    renamed class to plot_summary() from plot_1D(), since it does more now (and I will add more functionality to make 2D plots)
    
    tried unsuccessfully to plot subplots in large and each in its separate figure (commented out)
    
    tried a lot on the correlation matrix plotting and kept this code commented out for future reference
    
    adjusted main() to reflect these changes -> output html file looks fine.

--- visualisation.py
+++ visualisation.py
@@ -8,7 +8,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import sys
-from pandas.tools.plotting import scatter_matrix
+import pandas.tools.plotting as ptp
+from pandas.compat import lrange
+import seaborn as sns
 
 import html_output as ho
 
@@ -26,39 +28,115 @@ class html_picture_summary_df(ho.html_picture_summary):
     #def __init__(self, of_name, CSS=[]):
     #    ho.html_picture_summary.__init__(self, of_name, CSS)
 
-    def body_content(self, summary_struct, title, level=1): #level=1:<h1>, level=2:<h2>, level=3:<h3>
+    def body_content(self, summary_struct, title, level=1, append=True): #level=1:<h1>, level=2:<h2>, level=3:<h3>
         l = []
         l[len(l):] = ["<h%d>%s</h%d>" % (level, title, level)]
         for s in summary_struct:
             l[len(l):] = ['<img src="%s" alt="%s"/>' % (str(s.fig_path), str(s.label))] #html code
             l[len(l):] = ["%s, %s" % (str(s.fig_path), str(s.label))]
 
-        self._body_content = "\n".join(l)
+        if not append: self._body_content = ""
+        self._body_content += "\n".join(l)
 
 
-class plot_1D():
+class plot_summary():
     def __init__(self, df, outdir="./"):
         self._df = df #input dataframe from which to make plots
         if outdir[-1]!="/": outdir+="/"
         self._output_dir = outdir #where the PNGs are being produced
         self.list_fig_summary = []
 
-    def corr_plots(self):
+    def corr_plots(self, append=True):
+        """
+        uses scatter_matrix to plot all columns pair-wise against each other 
+        """
         plt.figure()
         #self._df.drop([x for ])
-        #scatter_matrix(self._df, alpha=0.2, figsize=(6, 6), diagonal='kde')
-        #scatter_matrix(self._df, alpha=0.2, diagonal='kde')
-        diagonal='kde'
-        scatter_matrix(self._df, alpha=0.2, diagonal=diagonal)
-        print(diagonal)
-        plt.savefig(self._output_dir + "correlation_matrix_%s.png" % diagonal)
+        #ptp.scatter_matrix(self._df, alpha=0.2, figsize=(6, 6), diagonal='kde')
+        #ptp.scatter_matrix(self._df, alpha=0.2, diagonal='kde')
+        #diagonal='kde'
+        #ptp.scatter_matrix(self._df, alpha=0.2, diagonal=diagonal)
+        #print(diagonal)
+        #plt.savefig(self._output_dir + "correlation_matrix_%s.png" % diagonal)
 
         diagonal='hist'
-        scatter_opt={'kind':'hexbin', 'color':'red'}
-        #scatter_opt={'color':'red'}
-        scatter_matrix(self._df, alpha=0.2, hspace=0.2, wspace=0.2, diagonal=diagonal, **scatter_opt)
-        print(diagonal)
-        plt.savefig(self._output_dir + "correlation_matrix_%s.png" % diagonal)
+        print(sys._getframe().f_code.co_name, diagonal)
+        #scatter_opt={'kind':'hexbin'}
+        off_diagonal_opt={'bins':'log'} #draw options for off-diagonal elements
+        #scatter_opt={}
+        #scatter_matrix(self._df, alpha=0.2, hspace=0.2, wspace=0.2, diagonal=diagonal, **scatter_opt)
+        #axes = ptp.scatter_matrix(self._df, alpha=0.2, diagonal=diagonal, **scatter_opt)
+        ptp.scatter_matrix(self._df, alpha=0.2, diagonal=diagonal, **off_diagonal_opt)
+        #scatter_matrix(self._df, alpha=0.2, diagonal=diagonal)
+
+        fs = fig_summary()   #fs.mean = average(df[var_name])
+        fs.label = "correlation matrix"
+        fs.fig_path = self._output_dir + "correlation_plots_%s.png" % diagonal
+        
+        plt.savefig(fs.fig_path)
+        if not append: self.list_fig_summary.clear()
+        self.list_fig_summary.append(fs)
+
+
+        #n=self._df.columns.size
+        #for i, a in zip(lrange(n), self._df.columns):
+        #    for j, b in zip(lrange(n), self._df.columns):
+        #        ax = axes[i, j]
+        #        print(ax)
+        #        plt.figure()
+        #        ax.plot()
+        #        print("correlation_matrix_%s_%s.png" % (ax.get_xlabel(), ax.get_ylabel()))
+        #        plt.savefig(self._output_dir + "correlation_matrix_%s_%s.png" % (ax.get_xlabel(), ax.get_ylabel()))
+
+    def corr_matrix(self, append=True):
+        """
+        produces correlation matrix
+        """
+        print(sys._getframe().f_code.co_name)
+        plt.figure()
+        corr_df = self._df.corr()
+        #print("corr_df: ")
+        #print(corr_df)
+        #print(corr_df.info())
+        #print(corr_df.describe())
+        #print ("len(corr_df): %d" % len(corr_df))
+        #corr_df.plot(, kind='scatter')
+        
+        #x, y, z = [], [], []
+        #buffer = []
+        #var_labels = []
+        #buffer = range(corr_df.columns.size)
+        #for i,label in enumerate(corr_df.columns):
+        #    for y_i, corr_xy in enumerate(corr_df[label].values):
+        #        x.append(i)
+        #        y.append(y_i)
+        #        z.append(corr_xy)
+
+        #plt.scatter(x,y, c=z, s=500, square=True, vmin=-1, vmax=1)
+        #plt.heatmap(x,y, c=z, s=500, square=True, vmin=-1, vmax=1)
+        #plt.scatter(x,y, c=z, s=500, vmax=1)
+        #plt.scatter(x,y, c=z, s=500)
+        #cb = plt.colorbar()
+        #plt.xlim(-1, 1)
+        #plt.ylim(-1, 1)
+
+        #
+        #sns.heatmap(corr_df, vmax=1, xticklabels=5, yticklabels=5, square=True) #plot only every 5th label, it 
+        sns.heatmap(corr_df, vmax=1, square=True)
+
+        #for i in range(len(x)):
+        #    print(x[i], y[i], z[i])
+
+        #    for y,label_y in enumerate(self._df.columns)
+        
+        fs = fig_summary()   #fs.mean = average(df[var_name])
+        fs.label = "correlation matrix"
+        fs.fig_path = self._output_dir + "correlation_matrix.png"
+        
+        plt.savefig(fs.fig_path)
+        if not append: self.list_fig_summary.clear()
+        self.list_fig_summary.append(fs)
+
 
     def perVar(self, var_name):
         """for each variable make a PNG and store some text"""
@@ -80,6 +158,7 @@ class plot_1D():
             #args={'log': True}
             #args = {'bins': 120}
             self._df[var_name].hist()
+            plt.xlabel(var_name)
             plt.suptitle = "%s" % var_name
             fs.xvar = var_name
             fs.yvar = "entries/bin"
@@ -104,17 +183,21 @@ def main():
     output_file = "/home/reinhold/data/ML/output_data/mytest.html"
     h = html_picture_summary_df(output_file, ["Nachmieter.css"])
 
-    x = plot_1D(train_df, "/home/reinhold/data/ML/output_data/")
+    x = plot_summary(train_df, "/home/reinhold/data/ML/output_data/")
 
-    print(train_df.info())
+    x.corr_plots(False) #scatter_matrix quite powerful, ignores string variables automatically
+    x.corr_matrix(True) 
+    h.body_content(x.list_fig_summary, "correlation matrix and correlation plots (columns pair-wise)", 2, True)
+
+    x.list_fig_summary.clear()
+
+    #print(train_df.info())
     #print type(train_df.columns)
-    #for vars in  train_df.columns:
-    #    print vars
-    #    x.perVar(vars)
+    for vars in  train_df.columns:
+        print(vars)
+        x.perVar(vars)
+    h.body_content(x.list_fig_summary, "1D plots of each variable", 2, True)
 
-    x.corr_plots() #scatter_matrix quite powerful, ignores string variables automatically
-
-    h.body_content(x.list_fig_summary, "1D plots of each variable", 2)
     h.loop("Titanic Training Set")
 
     
